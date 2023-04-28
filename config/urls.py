f81db8f06e2b6a4d399e7ca9a464cc67798321d8
Tea_ <vero.zero.xxx@gmail.com>
2023-04-28 13:28:31 +0900

    url,템플릿 생성

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from game import views
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("gameing/",views.gameing),
+    path("main/", admin.main),
+    path("over_win/", admin.over_win),
+    path("over_lose/", admin.over_lose),
 ]
